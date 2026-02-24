ea8d99dda9a7dd7246a5f978aa933cc5340b3e23
Project Contributor <you@example.com>
2026-02-25 00:40:16 +0800

    Fix 404 error by renaming index.jsx to main.jsx and updating entry points

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import './index.css';
 import App from './App';
 import { store } from './redux/store';
 
+console.log('Main entry point loaded');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -14,5 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </Router>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
